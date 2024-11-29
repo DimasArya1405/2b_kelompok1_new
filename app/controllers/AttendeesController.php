23fa4faa97c39afef01f18726c2844470ac32772
meytea <melitakhasanah@gmail.com>
2024-11-29 11:03:47 +0700

    Menambahkan relasi dengan tabel event

--- app/controllers/AttendeesController.php
+++ app/controllers/AttendeesController.php
@@ -1,14 +1,15 @@
 <?php
 // app/controllers/UserController.php
 require_once '../app/models/Attendees.php';
+require_once '../app/models/Events.php';
 
-class AttendeesController {
+class AttendeesController extends Attendees{
     private $attendeesModel;
 
     public function __construct() {
         $this->attendeesModel = new Attendees();
     }
-    
+
     public function index() {
         $attendees = $this->attendeesModel->getAllAttendees();
         require_once '../app/views/attendees/index.php';
@@ -20,20 +21,19 @@ class AttendeesController {
     }
 
     public function store() {
+        $id_events = $_POST['id_events'];
         $nama_peserta = $_POST['nama_peserta'];
         $email = $_POST['email'];
         $nomor_telepon = $_POST['nomor_telepon'];
-        $acara = $_POST['acara'];
-        $this->attendeesModel->add($nama_peserta, $email, $nomor_telepon, $acara);
+        $this->attendeesModel->add($id_events,$nama_peserta, $email, $nomor_telepon);
         header('Location: /attendees/index');
     }
-    // Show the edit form with the user data
     public function edit($id) {
         $user = $this->attendeesModel->find($id); // Assume find() gets user by ID
-        require_once __DIR__ . '/../views/attendees/edit.php';
-    }
+        $attendees = $this->attendeesModel->getAllEvents(); // Ambil data kategori
+        require_once '../app/views/attendees/edit.php';
+}
 
-    // Process the update request
     public function update($id, $data) {
         $updated = $this->attendeesModel->update($id, $data);
         if ($updated) {
