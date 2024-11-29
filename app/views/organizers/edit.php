c132ac8bf5e5aab988f4fa828cfdac734b5ecd2a
AlvaRezal123 <ralfa9339@gmail.com>
2024-11-29 10:48:23 +0700

    menambahkan dropdwon dari event dan tabel relasi

--- app/views/organizers/edit.php
+++ app/views/organizers/edit.php
@@ -1,7 +1,5 @@
-<!-- app/views/user/edit.php -->
 <?php require_once '../public/header.php'; ?>
 <?php require_once '../public/main.php'; ?>
-
 
 <div class="flex flex-col justify-center items-center w-full">
     <div class="ml-64 -mt-5 py-5 flex flex-col justify-center items-center">
@@ -10,24 +8,30 @@
         </h1>
         <div class="btn-create mt-6">
             <a href="/events/index" class="flex gap-2 bg-red-600 rounded-md w-40 py-2 justify-center items-center gap-1 hover:bg-red-700 transition-duration-300 ease-in-out text-white">
-            <i class="bi bi-backspace"></i>
+                <i class="bi bi-backspace"></i>
                 Kembali
             </a>
         </div>
-    <form action="/organizers/update/<?php echo $organizers['id_nama_penyelenggara']; ?>" method="POST">
-        <label for="nama_penyelenggara">Nama Penyelenggara:</label>
-        <input type="text" id="nama_penyelenggara" name="nama_penyelenggara" value="<?php echo $organizers['nama_penyelenggara']; ?>" required>
-        <br>
-        <label for="kontak">kontak:</label>
-        <input type="text" id="kontak" name="kontak" value="<?php echo $organizers['kontak']; ?>" required>
-        <br>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" value="<?php echo $organizers['email']; ?>" required>
-        <br>
-        <button type="submit">Update</button>
-    </form>
-    <a href="/organizers/index">Back to List</a>
-    </div></div>
-<?php require_once '../public/navbar.php'; ?>
+        <form action="/organizers/update/<?php echo $user['id_nama_penyelenggara']; ?>" method="POST">
+            <label for="nama_penyelenggara">Nama Penyelenggara:</label>
+            <input type="text" id="nama_penyelenggara" name="nama_penyelenggara" value="<?php echo $user['nama_penyelenggara']; ?>" required>
+            <label for="kontak">Kontak:</label>
+            <input type="text" id="kontak" name="kontak" value="<?php echo $user['kontak']; ?>" required>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" value="<?php echo $user['email']; ?>" required>
+            <label for="acara">Acara</label>
+            <select id="select" name="id_events" required>
+                <option value="" selected disabled>Pilih Acara</option>
+                <?php foreach ($organizers as $data): ?>
+                    <option value="<?= htmlspecialchars($data['id_events']) ?>" <?= $data['id_events'] == $user['id_events'] ? 'selected' : '' ?>>
+                        <?= htmlspecialchars($data['nama_acara']) ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+            <button type="submit" class=" w-24 py-2 bg-green-600 text-white hover:bg-green-800 rounded-md transition duration-300 ease-in-out mt-4">Update</button>
+        </form>
+    </div>
+</div>
 
+<?php require_once '../public/navbar.php'; ?>
 <?php require_once '../public/footer.php'; ?>
