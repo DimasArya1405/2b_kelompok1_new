3b139d15a3ab48ba5979e07a1d13d2caa5c4e11a
DimasArya1405 <dimasaryad55@gmail.com>
2024-11-30 09:57:12 +0700

    Menambahkan AOS animation

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -6,35 +6,35 @@
         <div class="text-center px-10 py-3 bg-white rounded-xl h-52 -mt-10 z-10 w-2/3 judul mb-5">
             <div class="welcome font-bold">SELAMAT DATANG</div>
             <p>Di Website Sistem Manajemen Acara Seni dan Budaya</p>
-            <div id="date-time"></div>
+            <div id="date-time" class=" text-xl"></div>
 
         </div>
         <!-- <div  class="flex flex-col justify-center items-center w-3">
     <div class="ml-64 -mt-5 py-5 flex flex-col justify-center items-center w-3"></div>
 </div> -->
         <div class="con flex items-center justify-center p-5 w-full gap-5">
-            <div class="card w-52 h-96 bg-gradient-to-br from-purple-500 p-5 to-purple-300  flex-col flex justify-center items-center gap-5 rounded-lg">
+            <div data-aos="fade-up" data-aos-duration="1000" class=" card w-52 h-96 bg-gradient-to-br from-purple-500 p-5 to-purple-300  flex-col flex justify-between items-center gap-5 rounded-lg">
                 <div class="font-bold">EVENTS</div>
                 <img src="../../../public/assets/event.png" alt="">
                 <p class="text-center text-sm font-normal">Acara seni dan budaya yang mencakup jadwal dan lokasi acara.</p>
                 <a href='/events/index' class="py-2 bg-purple-700 hover:bg-purple-900 text-white rounded-md cursor-pointer transition duration-300 ease-in-out">View</a>
             </div>
-            <div class="card w-52 h-96 bg-gradient-to-br from-purple-500 p-5 to-purple-300  flex-col flex justify-center items-center gap-5 rounded-lg">
+            <div data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000" class="card w-52 h-96 bg-gradient-to-br from-purple-500 p-5 to-purple-300  flex-col flex justify-between items-center gap-5 rounded-lg">
                 <div class="font-bold">ORGANIZERS</div>
-                <img src="../../../public/assets/organisasi.png" alt="">
+                <img src="../../../public/assets/organisasi.png" alt="" class="mb-4 mt-3">
                 <p class="text-center text-sm font-normal">Berisi penyelenggara, organisasi, kontak dan email</p>
                 <a href="/organizers/index" class="py-2 bg-purple-700 hover:bg-purple-900 text-white rounded-md cursor-pointer transition duration-300 ease-in-out">View</a>
             </div>
-            <div class="card w-52 h-96 bg-gradient-to-br from-purple-500 p-5 to-purple-300  flex-col flex justify-center items-center gap-5 rounded-lg">
+            <div data-aos="fade-up" data-aos-delay="400" data-aos-duration="1000" class="card w-52 h-96 bg-gradient-to-br from-purple-500 p-5 to-purple-300  flex-col flex justify-between items-center gap-5 rounded-lg">
                 <div class="font-bold">ATTENDEES</div>
                 <img src="../../../public/assets/attendees.png" alt="">
                 <p class="text-center text-sm font-normal">Peserta yang terdaftar untuk menghadiri acara seni dan budaya </p>
                 <a href="/attendees/index" class="py-2 bg-purple-700 hover:bg-purple-900 text-white rounded-md cursor-pointer transition duration-300 ease-in-out">View</a>
             </div>
-            <div class="card w-52 h-96 bg-gradient-to-br from-purple-500 p-5 to-purple-300  flex-col flex justify-center items-center gap-5 rounded-lg">
+            <div data-aos="fade-up" data-aos-delay="600" data-aos-duration="1000" class="card w-52 h-96 bg-gradient-to-br from-purple-500 p-5 to-purple-300  flex-col flex justify-between items-center gap-5 rounded-lg">
                 <div class="font-bold">SPONSHORSHIP</div>
                 <img src="../../../public/assets/sponsor.png" alt="">
-                <p class="text-center text-sm font-normal">Pihak yang memberikan dana atau fasilitas untuk acara </p>
+                <p class="text-center text-sm font-normal">Pihak yang memberikan dana & fasilitas untuk acara </p>
                 <a href="sponsorships/index" class="py-2 bg-purple-700 hover:bg-purple-900 text-white rounded-md cursor-pointer transition duration-300 ease-in-out">View</a>
             </div>
         </div>
